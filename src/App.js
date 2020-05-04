d60a6edb3d0f7520bb78919976fcc4abd767e688
Daniel <arangomonsalve.daniel@gmail.com>
2020-05-04 15:37:46 -0500

    store, reducer,action

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
+//Redux
+import {Provider} from 'react-redux'
+import store from "./store";
 
 function App() {
-  return (
-   <Router>
-     <Header/>
-     <div className="container mt-5">
-         <Switch>
-             <Route exact path='/' component={Productos}/>
-             <Route exact path='/productos/nuevo' component={NuevoProducto}/>
-             <Route exact path='/productos/editar/:id' component={EditarProducto}/>
-         </Switch>
-     </div>
-   </Router>
-  );
+    return (
+        <Router>
+            <Provider store={store}>
+                <Header/>
+                <div className="container mt-5">
+                    <Switch>
+                        <Route exact path='/' component={Productos}/>
+                        <Route exact path='/productos/nuevo' component={NuevoProducto}/>
+                        <Route exact path='/productos/editar/:id' component={EditarProducto}/>
+                    </Switch>
+                </div>
+            </Provider>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,8 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+//Actions de redux
+import {crearNuevoProductoAction} from '../actions/productoActions'
+import {useDispatch} from "react-redux";
 
 const NuevoProducto = () => {
+
+    //State del componente
+    const [nombre, guardarNombre] = useState('')
+    const [precio, guardarPrecio] = useState(0)
+
+    //Utilizar useDispatch y te crea una funcion
+    const dispatch = useDispatch();
+
+    //Mandar llamar el action de productoAction
+    const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto))
+
+    //Cuando el usuario haga submit
+    const submitNuevoProducto = e => {
+        e.preventDefault();
+
+        //Validar formulario
+        if (nombre.trim() === '' || precio <= 0) {
+            return;
+        }
+
+        //Si no hay errores
+
+        //Crear el nuevo producto
+        agregarProducto({
+            nombre,
+            precio
+        })
+    }
+
     return (
-       <h1>Desde nuevo producto</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar Nuevo Producto
+                        </h2>
+
+                        <form
+                            onSubmit={submitNuevoProducto}
+                        >
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    className='form-control'
+                                    placeholder='Nombre Producto'
+                                    name={'nombre'}
+                                    value={nombre}
+                                    onChange={e => guardarNombre(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input
+                                    type="number"
+                                    className='form-control'
+                                    placeholder='Precio Producto'
+                                    name={'precio'}
+                                    value={precio}
+                                    onChange={e => guardarPrecio(Number(e.target.value))}
+                                />
+                            </div>
+                            <button
+                                type={'submit'}
+                                className='btn btn-primary font-weight-bold text-uppercase
+                               d-block w-100'
+                            >Agregar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
